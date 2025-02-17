996a18c84c2069ab6d039a4676cdf20a75762fa2
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-02-18 00:01:48 +0530

    updated cors policy

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
