8a64ab972374822228e0e2c43329880e9d56a5cf
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-02-18 21:53:18 +0530

    updated cors to allow only deployed frontend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: "https://gptwebcontainerfrontend2.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
