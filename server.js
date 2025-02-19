f5631eafc850c0f24dd637ee66a757b1bb9314dc
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-02-19 18:52:19 +0530

    Added cors origin

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://gptwebcontainerfrontend2.onrender.com",
-      "https://gptwebcontainerfrontend3.onrender.com",
+      "https://webcontainerfrontend.netlify.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
