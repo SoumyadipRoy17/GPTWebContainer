54a3c08b585419a986267bb233935bff1cf31d15
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-05-12 09:50:06 +0530

    allow cross origin for deployed frontend

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ connect();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://webcontainerfrontend.netlify.app",
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
