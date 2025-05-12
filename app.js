52dedd335a4a21ad2ac6afef60e9610a2a28fc49
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-05-12 10:37:54 +0530

    .

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ connect();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
