5e75edfec7bd0958900c17eaf693efd08717d3d5
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-05-12 10:43:06 +0530

    .

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ connect();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://webcontainerfrontend.netlify.app/",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
